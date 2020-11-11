794dc7036b886659a3f9a5288958032587110909
Kevin Basele <kevinbasele@hotmail.se>
2020-11-11 09:51:49 +0100

    fixat landingpange och om ig

---
+++ app.js
@@ -0,0 +1,153 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    new Page()
+
+});
+
+class Page{
+    constructor(){
+        this.body = document.querySelector(".wrapper");
+        this.body.addEventListener("mousemove", this.shadow);
+        this.ShowOnlyPage("ladning-page");
+        this.changePage();
+        this.count = 0; 
+    }
+    
+    //Bestämmer vilken sida som ska visas
+    ShowOnlyPage(page ){
+        const pages = document.querySelectorAll(".pages")
+        for(let current of pages){
+            if(current.getAttribute("id") != page){
+                current.classList.add("hidden")
+            }
+            
+        }
+
+    }
+
+    //Skugga följer muspekaren
+    shadow(e){
+        const body = document.querySelector(".wrapper")
+        const landingPage = document.querySelector(".content-ladning-page")
+        const firstname = document.querySelector(".content-ladning-page-fname")
+        const lastname = document.querySelector(".content-ladning-page-lname")
+        const stretch =  100; 
+        
+
+        const {offsetWidth: width, offsetheight: height} = body
+        let {offsetX: x, offsetY: y} = e; 
+    
+        if(this !== e.target){
+            x = x + e.target.offsetLeft;
+            y = y + e.target.offsetTop; 
+        };
+    
+        const xStretch = Math.round((x /width * stretch) - (stretch / 2)); 
+        const yStretch = Math.round((y /width * stretch) - (stretch / 2)); 
+        
+        firstname.style.textShadow=`${xStretch}px ${yStretch}px 9px rgba(0, 0, 0, 0.15)`; 
+        lastname.style.textShadow=`${xStretch}px ${yStretch}px 9px rgba(0, 0, 0, 0.15)`; 
+
+    }
+
+    //Byter sida
+    changePage(){  
+        const navbar = document.querySelectorAll(".wrapper-footer li")
+        const firstname = document.querySelector(".content-ladning-page-fname")
+        const lastname = document.querySelector(".content-ladning-page-lname")
+        const landingPage = document.querySelector(".content-ladning-page")
+
+
+        let count = 0; 
+        for(let current of navbar){
+            const pages = document.querySelectorAll(".pages")
+
+            current.addEventListener("click", (e)=>{
+                count++
+                const that = this;
+                //Första sidbytet
+                if(count<2){
+                    if(current.getAttribute("class") == "ladning-page"){
+                        count = 0; 
+                        
+                    }
+                    else{
+                        console.log(current)
+                        firstname.setAttribute("id", "firstPageAnimation");
+                        lastname.setAttribute("id", "firstPageAnimation");
+                        landingPage.setAttribute("id", "fadeoutPage")
+                        
+                        setTimeout(function(){
+                            that.ShowOnlyPage(current);
+                            for(const cur of pages){
+                                if(cur.getAttribute("id") == current.getAttribute("class")){
+                                    cur.classList.remove("hidden")
+                                }
+                            }
+                        }, 1500)  
+                    }
+                }
+
+                //Byter efter första bytet
+                if(count>1){
+                    console.log(current)
+                    this.ShowOnlyPage(current);
+                    for(const cur of pages){
+                        if(cur.getAttribute("id") == current.getAttribute("class")){
+                            cur.classList.remove("hidden")
+                            cur.classList.remove("firstPageAnimation")
+                        }
+                    }       
+
+                }
+            })
+        }
+    }
+}
+
+
+const carouselSlide = document.querySelector(".carousel-slide")
+const carouselslidevideos = document.querySelectorAll(".carousel-slide-video ")
+
+//buttons
+const nextBtn = document.querySelector(".fa-chevron-right")
+let prevBtn = document.querySelector(".fa-chevron-left")
+
+//Counter
+
+let counter = 1; 
+const size = carouselslidevideos[0].clientWidth;
+
+carouselSlide.style.transform ="translateX(" + (-size * counter ) + "px";
+
+//button Listeners
+
+nextBtn.addEventListener("click", ()=>{
+    if(counter >= carouselslidevideos.length-1)return;
+
+    carouselSlide.style.transition ="transform 0.7s ease-in-out";
+    counter++; 
+    carouselSlide.style.transform ="translateX(" + (-size * counter ) + "px";
+    
+})
+
+prevBtn.addEventListener("click", ()=>{
+    if(counter <= 0)return 
+    carouselSlide.style.transition ="transform 0.7s ease-in-out";
+    counter--; 
+    carouselSlide.style.transform ="translateX(" + (-size * counter ) + "px";
+})
+
+carouselSlide.addEventListener("transitionend", ()=>{
+    if(carouselslidevideos[counter].id == "lastClone"){
+        carouselSlide.style.transition = "none"
+        counter = carouselslidevideos.length -2;
+        carouselSlide.style.transform ="translateX(" + (-size * counter ) + "px";
+
+    }
+    if(carouselslidevideos[counter].id == "firstClone"){
+        carouselSlide.style.transition = "none"
+        counter = carouselslidevideos.length -counter;
+        carouselSlide.style.transform ="translateX(" + (-size * counter ) + "px";
+
+    }
+})
